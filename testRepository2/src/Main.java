38ed68b63769440e9dd462166c197761d8f7ee0c
voyki <voy.killian@gmail.com>
2024-04-17 17:18:33 +0200

    Fifth commit

--- testRepository2/src/Main.java
+++ testRepository2/src/Main.java
@@ -8,6 +8,18 @@ public class Main {
         System.out.println("Commit number 4...");
         System.out.println("Commit number 5...");
 
+        System.out.println("Commit number 1...");
+        System.out.println("Commit number 2...");
+        System.out.println("Commit number 3...");
+        System.out.println("Commit number 4...");
+        System.out.println("Commit number 5...");
+
+        System.out.println("Commit number 1...");
+        System.out.println("Commit number 2...");
+        System.out.println("Commit number 3...");
+        System.out.println("Commit number 4...");
+        System.out.println("Commit number 5...");
+
         System.out.println("... ??? ...");
     }
 }
